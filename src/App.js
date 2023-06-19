ba7e8782838c3b91068a2bfaa779d86fd15a5d6f
Javaco95 <127981406+Javaco95@users.noreply.github.com>
2023-06-18 23:36:55 -0600

    170623

--- src/App.js
+++ src/App.js
@@ -139,133 +139,242 @@
 // }
 // }
 
+// import "./components/style/main.scss";
+// import React, { Component, useEffect, useState } from "react";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import axios from 'axios';
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTrash, faSignOutAlt, faEdit, faBan } from "@fortawesome/free-solid-svg-icons";
+
+// import NavigationContainer from "./components/navigation/navigation-container";
+// import Home from "./components/pages/home";
+// import About from "./components/pages/about";
+// import Contact from "./components/pages/contact";
+// import Blog from "./components/pages/blog";
+// import BlogDetail from "./components/pages/blog-detail";
+// import PortfolioManager from "./components/pages/portfolio-manager";
+// import PortfolioDetail from "./components/portfolio/portfolio-detail";
+// import Auth from "./components/pages/auth";
+// import NoMatch from "./components/pages/no-match";
+
+// library.add(faTrash, faSignOutAlt, faEdit, faBan);
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       loggedInStatus: "NOT_LOGGED_IN"
+//     };
+
+//     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+//     this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+//     this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
+//   }
+
+//   componentDidMount() {
+//     // Check if an authentication token exists in local storage
+//     const authToken = localStorage.getItem('authToken');
+//     if (authToken) {
+//       this.setState({ loggedInStatus: "LOGGED_IN" });
+//     }
+  
+//     //Check the login status
+//     this.checkLoginStatus();
+//   }
+
+//   handleSuccessfulLogin(authToken) {
+//     // Store the authentication token in local storage
+//     localStorage.setItem('authToken', authToken);
+  
+//     this.setState({
+//       loggedInStatus: "LOGGED_IN"
+//     });
+//   }
+
+//   handleUnsuccessfulLogin() {
+//     this.setState({
+//       loggedInStatus: "NOT_LOGGED_IN"
+//     });
+//   }
+
+//   handleSuccessfulLogout() {
+//     // Remove the authentication token from local storage
+//     localStorage.removeItem('authToken');
+
+//     this.setState({
+//       loggedInStatus: "NOT_LOGGED_IN"
+//     });
+//   }
+
+//   checkLoginStatus() {
+//     return axios
+//       .get("https://api.devcamp.space/logged_in", { withCredentials: true })
+//       .then((response) => {
+//         const loggedIn = response.data.logged_in;
+//         if (loggedIn) {
+//           this.setState({ loggedInStatus: "LOGGED_IN" });
+//         } else {
+//           this.setState({ loggedInStatus: "NOT_LOGGED_IN" }, () => {
+//             localStorage.removeItem('authToken');
+//           });
+//         }
+//       })
+//       .catch((error) => {
+//         console.log("Error", error);
+//       });
+//   }
+  
+
+//   authorizedPages() {
+//     return [
+//       <Route key="portfolio-manage" path="/portfolio-manager" element={<PortfolioManager />} />
+//     ];
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <Router>
+//           <div>
+//             <NavigationContainer
+//               loggedInStatus={this.state.loggedInStatus}
+//               handleSuccessfulLogout={this.handleSuccessfulLogout}
+//             />
+  
+//             <h2>{this.state.loggedInStatus}</h2>
+  
+//             <Routes>
+//               <Route exact path="/" element={<Home />} />
+//               <Route
+//                 path="/auth"
+//                 element={
+//                   <Auth
+//                     {...this.props}
+//                     handleSuccessfulLogin={this.handleSuccessfulLogin}
+//                     handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+//                     history={this.props.history}
+//                   />
+//                 }
+//               />
+//               <Route path="/about-me" element={<About />} />
+//               <Route path="/contact" element={<Contact />} />
+//               <Route path="/blog" element={<Blog />} />
+//               <Route path="/b/:slug" element={<BlogDetail />} />
+//               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
+//               <Route exact path="/portfolio/:slug" element={<PortfolioDetail/>} />
+//               <Route path="/:slug" element={<NoMatch />} />
+//             </Routes>
+//           </div>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }  
+
 import "./components/style/main.scss";
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit, faBan } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./components/navigation/navigation-container";
 import Home from "./components/pages/home";
 import About from "./components/pages/about";
 import Contact from "./components/pages/contact";
 import Blog from "./components/pages/blog";
+import BlogDetail from "./components/pages/blog-detail";
 import PortfolioManager from "./components/pages/portfolio-manager";
 import PortfolioDetail from "./components/portfolio/portfolio-detail";
 import Auth from "./components/pages/auth";
 import NoMatch from "./components/pages/no-match";
 
-library.add(faTrash, faSignOutAlt);
+library.add(faTrash, faSignOutAlt, faEdit, faBan);
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedInStatus: "NOT_LOGGED_IN"
-    };
+const App = () => {
+  const [loggedInStatus, setLoggedInStatus] = useState("NOT_LOGGED_IN");
 
-    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
-    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
-    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
-  }
-
-  componentDidMount() {
-    // Check if an authentication token exists in local storage
+  useEffect(() => {
     const authToken = localStorage.getItem('authToken');
     if (authToken) {
-      this.setState({ loggedInStatus: "LOGGED_IN" });
+      setLoggedInStatus("LOGGED_IN");
     }
-  
-    //Check the login status
-    this.checkLoginStatus();
-  }
+    checkLoginStatus();
+  }, []);
 
-  handleSuccessfulLogin(authToken) {
-    // Store the authentication token in local storage
+  const handleSuccessfulLogin = (authToken) => {
     localStorage.setItem('authToken', authToken);
-  
-    this.setState({
-      loggedInStatus: "LOGGED_IN"
-    });
-  }
+    setLoggedInStatus("LOGGED_IN");
+  };
 
-  handleUnsuccessfulLogin() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
-    });
-  }
+  const handleUnsuccessfulLogin = () => {
+    setLoggedInStatus("NOT_LOGGED_IN");
+  };
 
-  handleSuccessfulLogout() {
-    // Remove the authentication token from local storage
+  const handleSuccessfulLogout = () => {
     localStorage.removeItem('authToken');
+    setLoggedInStatus("NOT_LOGGED_IN");
+  };
 
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
-    });
-  }
-
-  checkLoginStatus() {
+  const checkLoginStatus = () => {
     return axios
       .get("https://api.devcamp.space/logged_in", { withCredentials: true })
       .then((response) => {
         const loggedIn = response.data.logged_in;
         if (loggedIn) {
-          this.setState({ loggedInStatus: "LOGGED_IN" });
+          setLoggedInStatus("LOGGED_IN");
         } else {
-          this.setState({ loggedInStatus: "NOT_LOGGED_IN" }, () => {
-            localStorage.removeItem('authToken');
-          });
+          setLoggedInStatus("NOT_LOGGED_IN");
+          localStorage.removeItem('authToken');
         }
       })
       .catch((error) => {
         console.log("Error", error);
       });
-  }
-  
+  };
 
-  authorizedPages() {
+  const authorizedPages = () => {
     return [
       <Route key="portfolio-manage" path="/portfolio-manager" element={<PortfolioManager />} />
     ];
-  }
+  };
 
-  render() {
-    return (
-      <div className="container">
-        <Router>
-          <div>
-            <NavigationContainer
-              loggedInStatus={this.state.loggedInStatus}
-              handleSuccessfulLogout={this.handleSuccessfulLogout}
+  return (
+    <div className="container">
+      <Router>
+        <div>
+          <NavigationContainer
+            loggedInStatus={loggedInStatus}
+            handleSuccessfulLogout={handleSuccessfulLogout}
+          />
+
+          <h2>{loggedInStatus}</h2>
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              path="/auth"
+              element={
+                <Auth
+                  handleSuccessfulLogin={handleSuccessfulLogin}
+                  handleUnsuccessfulLogin={handleUnsuccessfulLogin}
+                />
+              }
             />
-  
-            <h2>{this.state.loggedInStatus}</h2>
-  
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route
-                path="/auth"
-                element={
-                  <Auth
-                    {...this.props}
-                    handleSuccessfulLogin={this.handleSuccessfulLogin}
-                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
-                    history={this.props.history}
-                  />
-                }
-              />
-              <Route path="/about-me" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/blog" element={<Blog />} />
-              {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
-              <Route exact path="/portfolio/:slug" element={<PortfolioDetail/>} />
-              <Route path="/:slug" element={<NoMatch />} />
-            </Routes>
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}  
+            <Route path="/about-me" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/b/:slug" element={<BlogDetail />} /> //elimine slug
+            {loggedInStatus === "LOGGED_IN" ? authorizedPages() : null}
+            <Route exact path="/portfolio/:slug" element={<PortfolioDetail />} /> 
+            <Route path="/:slug" element={<NoMatch />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,101 @@
+// import React, { Component } from "react";
+// import axios from "axios";
+// import { useNavigate, Link, useParams } from "react-router-dom";
+// import { useEffect, useCallback } from "react";
+
+// export default class BlogDetail extends Component {
+//   constructor(props) {
+//     super(props);
+    
+//     const  = (props) => {
+//         const navigate = useNavigate();
+//         const { id, slug } = useParams();
+//     this.state = {
+//       currentId: this.props.match.params.slug,
+//       blogItem: {}
+//     };
+//   }
+
+//   getBlogItem() {
+//     axios
+//       .get(
+//         `https://javaco95.devcamp.space/portfolio/portfolio_blogs/${this.props.match.params.slug}`
+//       )
+//       .then(response => {
+//         this.setState({
+//           blogItem: response.data.portfolio_blog
+//         });
+//       })
+//       .catch(error => {
+//         console.log("getBlogItem error", error);
+//       });
+//   }
+  
+
+//   componentDidMount() {
+//     this.getBlogItem();
+//   }
+
+//   render() {
+//     const {
+//       title,
+//       content,
+//       featured_image_url,
+//       blog_status
+//     } = this.state.blogItem;
+
+//     return (
+//       <div>
+//         <h1>{title}</h1>
+//         <img src={featured_image_url} />
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import "./blog.scss";
+
+const BlogDetail = () => {
+  const { slug } = useParams();
+  const [blogItem, setBlogItem] = useState(null);
+
+  useEffect(() => {
+    const getBlogItem = async () => {
+      try {
+        const response = await axios.get(
+          `https://javaco95.devcamp.space/portfolio/portfolio_blogs/${slug}`
+        );
+        setBlogItem(response.data.portfolio_blog);
+      } catch (error) {
+        console.log("getBlogItem error", error);
+      }
+    };
+
+    getBlogItem();
+  }, [slug]);
+
+  if (!blogItem) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, content, featured_image_url, blog_status } = blogItem;
+
+  return (
+    <div className="blog-container">
+        <div className="content-container">
+        <h1>{title}</h1>
+
+        <div className="featured-image-wrapper">
+        <img src={featured_image_url} alt={title} />
+        </div>
+
+        <div className="content">{content}</div>
+        </div>
+        </div>
+  );
+};
+
+export default BlogDetail;
